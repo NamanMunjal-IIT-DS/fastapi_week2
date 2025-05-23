5f12287754ce71fde5f76b2ac2abd68c20188137
NamanMunjal17 <74naman@gmail.com>
2025-05-23 16:33:16 +0530

    another cors

--- api/main.py
+++ api/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Use specific domain in production
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
