27e5f51438a6fb03cac0857497dad247abb2ec3d
NamanMunjal17 <74naman@gmail.com>
2025-05-23 15:01:08 +0530

    first commit

---
+++ api/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI,Query
+import json
+from pathlib import Path
+from typing import List
+
+app = FastAPI()
+
+DATA_FILE = Path(__file__).parent / "q-vercel-python.json"
+f=open(DATA_FILE, "r")
+data = json.load(f)
+f.close()
+    
+global d
+d=data
+
+@app.get("/marks")
+def get_marks(names: List[str] = Query(...)):
+    global d
+    marks=[]
+    for name in names:
+        for i in d:
+            if name == i["name"]:
+                marks.append(i["marks"])
+    return {"marks":marks}
